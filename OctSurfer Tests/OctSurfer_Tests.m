eb60a1cf1df1175c7fa4eb3a0e84958c6bcdd0e7
tang <fangshiyu2@gmail.com>
2014-07-21 16:17:33 +0800

    oc unit test

---
+++ OctSurfer Tests/OctSurfer_Tests.m
@@ -0,0 +1,36 @@
+//
+//  OctSurfer_Tests.m
+//  OctSurfer Tests
+//
+//  Created by tang on 14-7-21.
+//  Copyright (c) 2014年 yo_waka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OctSurfer_Tests : XCTestCase
+
+@end
+
+@implementation OctSurfer_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    int a = 1;
+    int b = 1;
+    XCTAssertEqual(a, b, @"");
+}
+
+@end
